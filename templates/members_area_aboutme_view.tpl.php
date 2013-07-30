bada7034d6da94545fd565e37ff5324627839da1
Dev <lilian.codreanu@gmail.com>
2013-07-30 23:14:49 +0300

    test user details about

--- templates/members_area_aboutme_view.tpl.php
+++ templates/members_area_aboutme_view.tpl.php
@@ -16,13 +16,28 @@ if ( !defined('INCLUDED') ) { die("Access Denied"); }
     </div>
     <div id="about_me_content">
         <div>
-            Location : <?=$user_details['city']. ' '?>
+            Location : <?=$user_details['city'] . ', ' . $user_details['address']?>
         </div>
         <div>
             Also Find Me on
+            <?php if (!empty($user_details['facebook_link'])) :?>
+                <label class="facebook">
+                    <a href="<?=$user_details['facebook_link']?>" target="_blank">Facebook</a>
+                </label>
+            <?php endif;?>
+            <?php if (!empty($user_details['twitter_link'])) :?>
+                <label class="twitter">
+                    <a href="<?=$user_details['twitter_link']?>" target="_blank">Twitter</a>
+                </label>
+            <?php endif;?>
+            <?php if (!empty($user_details['google_link'])) :?>
+                <label class="google">
+                    <a href="<?=$user_details['google_link']?>" target="_blank">Google+</a>
+                </label>
+            <?php endif;?>
         </div>
         <div>
-            content
+            <?=$user_details['about_me']?>
         </div>
     </div>
 </div>
