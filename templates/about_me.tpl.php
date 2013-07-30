fdcb964b92f4b4a11eba21fdaa259fd0b23675a3
Lilian Codreanu <lilian.codreanu@gmail.com>
2013-07-30 15:17:02 +0300

    About me + gitignore .ide

---
+++ templates/about_me.tpl.php
@@ -0,0 +1,47 @@
+<?
+#################################################################
+## PHP Pro Bid v6.07															##
+##-------------------------------------------------------------##
+## Copyright �2007 PHP Pro Software LTD. All rights reserved.	##
+##-------------------------------------------------------------##
+#################################################################
+
+if ( !defined('INCLUDED') ) { die("Access Denied"); }
+?>
+
+<div class="view_about_page">
+    <h6 class="tittle_tp">  <?=MSG_MM_ABOUT_ME_PAGE;?> </h6>
+    <div class="view_about_content">
+        <div class="view_about_logo">
+            <div class="view_about_img">
+                <img src="http://alexlebedev27.files.wordpress.com/2012/07/china5.jpg"/>
+            </div>
+            <span>
+                <label>Name User</label>
+            </span>
+        </div>
+
+        <div class="view_about_info">
+            <a href="" class="location">my city</a>
+            <div class="inner">
+                <h5>Also find Me On:</h5>
+                <ul>
+                    <li class="a_facebook">
+                        <a href="https://facebook.com/tuvalutuvalutuvalu">Facebook</a>
+                    </li>
+                    <li class="a_twitter">
+                        <a href="https://twitter.com/tuvalutuvalutuvalu">Twitter</a>
+                    </li>
+                    
+                    <li>
+                        <a href="http://www.google.com" target="_blank">Website</a>
+                    </li>
+                </ul>
+                <p>
+                    Așezarea, în raport de meridiane și paralele, este următoarea: punctul cel mai nordic se află la 71° latitudine N, în peninsula Barrow (din Alaska), iar cel mai sudic la 55° latitudine S, în Capul Horn. Între cele două puncte se află 16.000 de km. Împreună cu insulele din nordul Americii de Nord, continentul se întinde până la 83° latitudine N, în nordul Groenlandei. Această insulă, care este cea mai mare din lume (peste 2 mil. km²), ține, din punct de vedere geografic, de America, dar politico-administrativ, de Danemarca.
+                    În longitudine, America de Nord este situată de o parte și de alta a meridianului de 100° V, iar America de Sud este situată de o parte și de alta a meridianului de 60° V. Lățimea maximă a ambelor continte este de 5.000 km (fără partea insulară din nord). Ecuatorul întretaie America de Sud prin partea sa nordică (aproximativ în lungul Amazonului). Tropicul Racului trece prin sudul Americii de Nord, iar Tropicul Capricornului traversează aproximativ centrul Americii de Sud.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
