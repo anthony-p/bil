ad0c225201502ebe70ea919c12800b059d8f3fa6
Dev <lilian.codreanu@gmail.com>
2013-07-31 09:04:58 +0300

    add avatart to about page

--- members_area.php
+++ members_area.php
@@ -3700,41 +3700,52 @@ else
 		if ($section == 'edit')
 		{
 
-			if (isset($_POST['form_aboutme_save']))
-			{
+			if (isset($_POST['form_aboutme_save'])) {
                 include_once('includes/generate_image_thumbnail.php');
                 var_dump($_FILES);
                 if (isset ($_FILES["avatar"]) ) {
-                    $ext = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
-                    var_dump($ext);
-                    $logo_file_name = '/images/partner_logos/' . md5($_POST["first_name"] . 'logo') . '.' . $ext;
-                    var_dump($logo_file_name);
-                    $upload_logo = generate_image_thumbnail(
-                        $_FILES["avatar"]["tmp_name"], '..' . $logo_file_name, 640, 600
+                    $allowed_image_mime_types = array(
+                        'image/gif',
+                        'image/jpeg',
+                        'image/pjpeg',
+                        'image/png',
+                        'image/tiff',
+                        'image/svg+xml'
                     );
-                    exec('chmod 0777 ../images/partner_logos/temp/' . md5($_SESSION["probid_user_id"] . 'logo') . '.*');
-                    exec('rm ../images/partner_logos/temp/' . md5($_SESSION["probid_user_id"] . 'logo') . '.*');
-                    $_POST["avatar"] = $logo_file_name;
-                    var_dump($_POST);
+                    if (in_array($_FILES["logo"]["type"], $allowed_image_mime_types)) {
+                        $ext = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+                        var_dump($ext);
+                        $logo_file_name = '/images/partner_logos/' . md5($_POST["first_name"] . 'logo') . '.' . $ext;
+                        var_dump($logo_file_name);
+                        $upload_logo = generate_image_thumbnail(
+                            $_FILES["avatar"]["tmp_name"], '..' . $logo_file_name, 640, 600
+                        );
+                        exec('chmod 0777 ../images/partner_logos/temp/' . md5($_POST["first_name"] . 'logo') . '.*');
+                        exec('rm ../images/partner_logos/temp/' . md5($_POST["first_name"] . 'logo') . '.*');
+                        $_POST["avatar"] = $logo_file_name;
+                        var_dump($_POST);
+
+                        $post_about_details = $db->rem_special_chars_array($_POST);
+                        $db->query("UPDATE bl2_users SET
+					    avatar='" . $post_about_details['avatar'] . "',
+                        about_me='" . $post_about_details['about_me'] . "',
+                        facebook_link='" . $post_about_details['facebook_link'] . "',
+                        twitter_link='" . $post_about_details['twitter_link'] . "',
+                        google_link='" . $post_about_details['google_link'] . "' WHERE
+                        id='" . $session->value('user_id') . "'");
+
+                        $template->set('msg_changes_saved', $msg_changes_saved);
+                        $user_details = $db->get_sql_row("SELECT * FROM bl2_users WHERE id=" . $session->value('user_id'));
+                        $template->set('user_details', $user_details);
+                    } else {
+                        $user_details= $post_about_details;
+                        $template->set('user_details', $user_details);
+                    }
                 }
-
-
-                //avatar='" . $post_about_details['avatar'] . "',
-                $post_about_details = $db->rem_special_chars_array($_POST);
-                $db->query("UPDATE bl2_users SET
-					avatar='" . $post_about_details['avatar'] . "',
-					about_me='" . $post_about_details['about_me'] . "',
-					facebook_link='" . $post_about_details['facebook_link'] . "',
-					twitter_link='" . $post_about_details['twitter_link'] . "',
-					google_link='" . $post_about_details['google_link'] . "' WHERE
-					id='" . $session->value('user_id') . "'");
-
-				$template->set('msg_changes_saved', $msg_changes_saved);
-			}
-
-            $user_details = $db->get_sql_row("SELECT * FROM bl2_users WHERE id=" . $session->value('user_id'));
-
-			$template->set('user_details', $user_details);
+			} else {
+                $user_details = $db->get_sql_row("SELECT * FROM bl2_users WHERE id=" . $session->value('user_id'));
+                $template->set('user_details', $user_details);
+            }
 
 			$members_area_page_content = $template->process('members_area_aboutme_edit.tpl.php');
 			$template->set('members_area_page_content', $members_area_page_content);
