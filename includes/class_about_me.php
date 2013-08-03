c48f3eda8a6c5ef6b22a82f977dd1d8cb9bca0f6
Dev <lilian.codreanu@gmail.com>
2013-08-03 23:48:32 +0300

    about page edit

--- includes/class_about_me.php
+++ includes/class_about_me.php
@@ -31,6 +31,7 @@ if (isset($_POST['form_aboutme_save'])) {
     } else {
         $_POST["avatar"] = $_POST["curr_avatar"];
         insertAboutUserDetails($_POST, $db, $user_id);
+        $template->set('msg_changes_saved', $msg_changes_saved);
         $user_details = $db->get_sql_row("SELECT * FROM bl2_users WHERE id=" . $user_id);
         $template->set('user_details', $user_details);
     }
