13b9b7a66644f8f261972bc688d3582dce9cda7f
Dev <lilian.codreanu@gmail.com>
2013-08-07 19:49:30 +0300

    ADD IN TRANSLATE MEMBERS MENU

--- templates/members_area_campaigns.tpl.php
+++ templates/members_area_campaigns.tpl.php
@@ -62,9 +62,9 @@ function submit_form(form_name) {
 
     <form id="search_by_name" accept="#">
         <select id="order_result" name="order" class="changeMe">
-            <option value="" selected="selected" class="order">Sort by</option>
-            <option value="ASC" class="order">Date asc</option>
-            <option value="DESC" class="order">Date desc</option>
+            <option value="" selected="selected" class="order"><?=MSG_SORT_BY?></option>
+            <option value="ASC" class="order"><?=MSG_DATE_ASC?></option>
+            <option value="DESC" class="order"><?=MSG_DATE_DESC?></option>
         </select>
         <div class="search-input">
             <input type="text" value="" placeholder="Find by name" name="keyword" id="name">
@@ -76,21 +76,21 @@ function submit_form(form_name) {
         <?php foreach( $campaigns_list as $row):?>
         <li id="li_<?php echo $row["user_id"]?>">
             <dl>
-                <dt>Campaign Name:</dt>
+                <dt><?=MSG_CAMPAIGN_NAME?>:</dt>
                 <dd><?php echo $row['name'];?></dd>
-                <dt>Create at:</dt>
+                <dt><?=MSG_CREATE_AT?>:</dt>
                 <dd><?php echo date("d.m.y",$row['end_date']);?></dd>
-                <dt>Closed on:</dt>
+                <dt><?=MSG_CLOSED_ON?>:</dt>
                 <dd><?php echo date("d.m.y",$row['reg_date']);?></dd>
-                <dt>Collected money</dt>
+                <dt><?=MSG_COLLECTED_MONEY?></dt>
                 <dd><?php echo $row['price'];?>$</dd>
             </dl>
             <div class="clear"></div>
             <fieldset>
                 <div class="campaignsButtons">
                     <a href="/<?php echo $row['username']; ?>" target="_blank"class="view">view</a>
-                    <a href="/campaigns,page,edit,section,<?php echo $row["user_id"]?>,campaign_id,members_area?keyword=#<?php echo $row['user_id'];?>;?>" class="edit">edit</a>
-                    <a href="/np/npdelete.php?np_userid=<?php echo $row["user_id"]?>" id="<?php echo $row["user_id"]?>" class="delete">delete</a>
+                    <a href="/campaigns,page,edit,section,<?php echo $row["user_id"]?>,campaign_id,members_area?keyword=#<?php echo $row['user_id'];?>;?>" class="edit"><?=MSG_MM_EDIT?></a>
+                    <a href="/np/npdelete.php?np_userid=<?php echo $row["user_id"]?>" id="<?php echo $row["user_id"]?>" class="delete"><?=MSG_DELETE?></a>
                 </div>
             </fieldset>
         </li>
