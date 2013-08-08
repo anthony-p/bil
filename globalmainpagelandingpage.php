48aec90a6726aeca2adb91185355a1c14f870024
Lilian Codreanu <lilian.codreanu@gmail.com>
2013-08-09 01:31:00 +0300

    show all error

---
+++ globalmainpagelandingpage.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Lilian Codreanu.
+ * User: Lilian Codreanu
+ * Date: 8/9/13
+ * Time: 1:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "OK!";
+?>
