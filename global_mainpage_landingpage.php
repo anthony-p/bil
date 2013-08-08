0b3b96e83682fdcb2df9ac838d3444e99b28333d
Lilian Codreanu <lilian.codreanu@gmail.com>
2013-08-09 01:09:04 +0300

    show all error

--- global_mainpage_landingpage.php
+++ global_mainpage_landingpage.php
@@ -1,1 +1,1 @@
-<?php//error_reporting(E_ALL);$adult_categories = array();var_dump("OK!!!!!!!!");?>
+<?php//error_reporting(E_ALL);$adult_categories = array();var_dump("OK!!!!!!!!");
