65340621cc38fd0d97e21255740eb9112295963e
Mark White <mwhite@bringitlocal.com>
2013-08-16 15:50:19 -0700

    Edited message user gets if they cancel a donation from paypal page

--- templates/donate_cancel.tpl.php
+++ templates/donate_cancel.tpl.php
@@ -1,3 +1,4 @@
 <?php if ( !defined('INCLUDED') ) { die("Access Denied"); }
 ?>
-You have been canceled your donation ...
+Oops...it looks like you've canceled your donation...<br> 
+If this was a mistake, please go back and try again. If we can help you with some problem please <a href="mailto:support@bringitlocal.com">contact us</a> and let us know.
