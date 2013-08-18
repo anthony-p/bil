a6bff716f9e8a3d7ccfeef39f266e43cbadebbf5
Inna <ina_18@mail.ru>
2013-08-18 20:59:36 +0300

    bug

--- templates/members_area_campaigns_main.tpl.php
+++ templates/members_area_campaigns_main.tpl.php
@@ -1,1 +1,17 @@
-Main Page!!!
+<div class="main_campaigns_page">
+    <h2>My Campaigns</h2>
+
+    <div>
+        <ul>
+            <li><span>Click here to edit your draft campaign</span>
+                <a href="/campaigns,page,edit,section,10336,campaign_id,members_area?keyword=#10336;?" class="edit">edit campaign</a>
+            </li>
+            <li><span>Click here to create new campaign</span>
+                <a href="/np/npregister.php" class="create">new campaign</a>
+            </li>
+            <li><span>Click here to view your campaigns</span>
+                <a href="/campaigns,page,live,section,members_area#8ec3489f027e" class="view">view all</a>
+            </li>
+        </ul>
+    </div>
+</div>
