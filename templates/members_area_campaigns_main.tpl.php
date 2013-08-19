f2404601bc8e0858d32304f82f9d02f869498e1d
Mark White <mwhite@bringitlocal.com>
2013-08-19 09:13:10 -0700

    some text additions and edits to the new My Campaigns section page

--- templates/members_area_campaigns_main.tpl.php
+++ templates/members_area_campaigns_main.tpl.php
@@ -3,12 +3,26 @@
 
     <div>
         <ul>
-            <li><span>Click here to edit your draft campaign</span>
-                <a href="/campaigns,page,drafts,section,members_area" class="edit">edit campaign</a>
-            </li>
-            <li><span>Click here to create new campaign</span>
+           <li><span>Click here to create a new campaign</span>
                 <a href="/np/npregister.php" class="create">new campaign</a>
             </li>
+        
+           <li><span>Click here to edit your draft campaigns</span>
+                <a href="/campaigns,page,drafts,section,members_area" class="edit">edit campaign</a>
+            </li>
+          
+            <li><span>Click here to edit your live campaigns</span>
+                <a href="/campaigns,page,live,section,members_area" class="edit">edit campaign</a>
+            </li>
+            
+            
+          <li><span>Click here to edit your closed campaigns</span>
+                <a href="/campaigns,page,closed,section,members_area" class="edit">edit campaign</a>
+            </li>
+            
+                       
+            
+          
             <li><span>Click here to view your campaigns</span>
                 <a href="/campaigns,page,live,section,members_area" class="view">view all</a>
             </li>
