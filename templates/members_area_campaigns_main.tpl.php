c65407503fc06c6e2a9305beb76a7964a4b28af9
Mark White <mwhite@bringitlocal.com>
2013-08-19 09:15:59 -0700

    some text additions and edits to the new My Campaigns section page

--- templates/members_area_campaigns_main.tpl.php
+++ templates/members_area_campaigns_main.tpl.php
@@ -11,10 +11,7 @@
                 <a href="/campaigns,page,drafts,section,members_area" class="edit">edit campaign</a>
             </li>
           
-            <li><span>Click here to edit your live campaigns</span>
-                <a href="/campaigns,page,live,section,members_area" class="edit">edit campaign</a>
-            </li>
-            
+                    
             
           <li><span>Click here to edit your closed campaigns</span>
                 <a href="/campaigns,page,closed,section,members_area" class="edit">edit campaign</a>
@@ -23,8 +20,8 @@
                        
             
           
-            <li><span>Click here to view your campaigns</span>
-                <a href="/campaigns,page,live,section,members_area" class="view">view all</a>
+            <li><span>Click here to view your live campaigns</span>
+                <a href="/campaigns,page,live,section,members_area" class="view">Live</a>
             </li>
         </ul>
     </div>
