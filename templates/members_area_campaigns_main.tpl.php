1343d4fef8f0b79caae87de7b91071dc815f9cbc
Lilian Codreanu <lilian.codreanu@gmail.com>
2013-08-19 13:41:35 +0300

    Make My Campaigns the default page for members area

--- templates/members_area_campaigns_main.tpl.php
+++ templates/members_area_campaigns_main.tpl.php
@@ -4,13 +4,13 @@
     <div>
         <ul>
             <li><span>Click here to edit your draft campaign</span>
-                <a href="/campaigns,page,edit,section,10336,campaign_id,members_area?keyword=#10336;?" class="edit">edit campaign</a>
+                <a href="/campaigns,page,drafts,section,members_area" class="edit">edit campaign</a>
             </li>
             <li><span>Click here to create new campaign</span>
                 <a href="/np/npregister.php" class="create">new campaign</a>
             </li>
             <li><span>Click here to view your campaigns</span>
-                <a href="/campaigns,page,live,section,members_area#8ec3489f027e" class="view">view all</a>
+                <a href="/campaigns,page,live,section,members_area" class="view">view all</a>
             </li>
         </ul>
     </div>
