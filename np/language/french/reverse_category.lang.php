bd440f8dae0a33c89f191b797f4eb0960b23a529
Lilian Codreanu <lilian.codreanu@gmail.com>
2013-08-20 11:51:19 +0300

    create language files for one test language besides english: french

---
+++ np/language/french/reverse_category.lang.php
@@ -0,0 +1,8 @@
+<?
+$reverse_category_lang[1]="Design &amp; Multimedia";
+$reverse_category_lang[2]="Finance &amp; Management";
+$reverse_category_lang[3]="Legal";
+$reverse_category_lang[4]="Sales &amp; Marketing";
+$reverse_category_lang[5]="Web &amp; Programming";
+$reverse_category_lang[6]="Writing &amp; Translation";
+?>
