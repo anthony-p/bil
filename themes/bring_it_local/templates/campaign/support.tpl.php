fc225aefb1ff730fdf0b1230141da1fc056fa54b
Lilian Codreanu <lilian.codreanu@gmail.com>
2013-08-21 20:33:27 +0300

    make click throu shopping functional

--- themes/bring_it_local/templates/campaign/support.tpl.php
+++ themes/bring_it_local/templates/campaign/support.tpl.php
@@ -7,63 +7,55 @@
  * Time: 1:28 PM
  */
 ?>
+<?php
+//var_dump($partners);
+$count = 0;
+$column = 4;
+$first = 0;
+?>
 <aside class="suport">
-    <div class="row shop">
-        <div class="viewall">
-            <div class="text">
-                <h5>Clickthrough shopping</h5>
-                <div class="slide">
-                    <ul><li class="border"></li></ul>
+    <?php foreach($partners as $parner):?>
+        <?php if ($count == 0): ?>
+            <div class="row shop">
+        <?php endif; ?>
+
+            <?php if ($first == 0): $first = 1;?>
+                <div class="viewall">
+                    <div class="text">
+                        <h5>Clickthrough shopping</h5>
+                        <div class="slide">
+                            <ul><li class="border"></li></ul>
+                        </div>
+                    </div>
+                    <a href=""> see all</a>
                 </div>
+            <?php endif; ?>
+
+        <?php if ($count == 0): ?>
+            <ul class="rows-list">
+        <?php endif; ?>
+
+            <li <?php if ($count == $column-1) echo 'class="last"' ?> >
+                <div class="img">
+                    <?php echo html_entity_decode($parner['advert_code']); ?>
+                </div>
+                <div class="info">
+                    <h3><?php echo trim($parner['name']) ?></h3>
+                    <span><?php echo (int) $parner['advert_pct']; ?>% giveback</span>
+                    <a href="<?php echo $parner['advert_url'];  ?>"> Shop on site now</a>
+                </div>
+            </li>
+
+
+
+        <?php if ($count == $column-1): $count = -1; ?>
+                </ul>
             </div>
-            <a href=""> see all</a>
-        </div>
-        <ul class="rows-list">
-            <li>
-                <div class="img">
-                    <img src="themes/bring_it_local/img/test1.png" />
-                </div>
-                <div class="info">
-                    <h3>Amazon</h3>
-                    <span>3% giveback</span>
-                    <a href=""> Shop on site now</a>
-                </div>
-            </li>
+        <?php endif; ?>
 
-            <li>
-                <div class="img">
-                    <img src="themes/bring_it_local/img/test2.png" />
-                </div>
-                <div class="info">
-                    <h3>Amazon</h3>
-                    <span>3% giveback</span>
-                    <a href=""> Shop on site now</a>
-                </div>
-            </li>
+        <?php $count++; ?>
+    <?php endforeach ?>
 
-            <li>
-                <div class="img">
-                    <img src="themes/bring_it_local/img/test1.png" />
-                </div>
-                <div class="info">
-                    <h3>Amazon</h3>
-                    <span>3% giveback</span>
-                    <a href=""> Shop on site now</a>
-                </div>
-            </li>
-
-            <li class="last" >
-                <div class="img">
-                    <img src="themes/bring_it_local/img/test2.png" />
-                </div>
-                <div class="info">
-                    <h3>Amazon</h3>
-                    <span>3% giveback</span>
-                    <a href=""> Shop on site now</a>
-                </div>
-            </li>
-        </ul>
-    </div>
 <!--    <div class="row ">-->
 <!--            <div class="viewall">-->
 <!--            <div class="text">-->
