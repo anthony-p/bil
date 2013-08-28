0b606af499bfeb22bd3c21b7dcfc7b8b2887ba9c
inna <ina_18@mail.ru>
2013-08-28 21:27:42 +0300

    contribution page

--- templates/members_area_contributions.tpl.php
+++ templates/members_area_contributions.tpl.php
@@ -1,1 +1,68 @@
-CONTRIBUTIONS PAGE
+<div class="contributions_page">
+    <h2>Contributions</h2>
+
+    <table>
+        <tr class="table_header">
+            <td><h4>Date</h4></td>
+            <td><h4>Campaign</h4></td>
+            <td><h4>Amount</h4></td>
+            <td><h4>Perk</h4></td>
+            <td><h4>Visibility</h4></td>
+        </tr>
+        <tr>
+            <td>Thu May 18.2013</td>
+            <td><span>fundrising_goal</span></td>
+            <td>10$</td>
+            <td>
+                <p>
+                    <span>Dniester River in Ukraine</span>
+                    Dniester River in Ukraine, and west up to and beyond the Carpathian Mountains in Romania. The inhabitants of this civilization, which lasted roughly from 5500 to 2750 BC, practiced agriculture, raised livestock, hunted, and made intricately designed pottery
+                </p>
+            </td>
+            <td>
+                <ul>
+                    <li><input type="radio" name="visibility" value=""><span>Name & Amount</span></li>
+                    <li><input type="radio" name="visibility" value=""><span>Name Only</span></li>
+                    <li><input type="radio" name="visibility" value=""><span>Anonymous</span></li>
+                </ul>
+            </td>
+         </tr>
+        <tr>
+            <td>Thu May 18.2013</td>
+            <td><span>fundrising_goal</span></td>
+            <td>10$</td>
+            <td>
+                <p>
+                    <span>Dniester River in Ukraine</span>
+                    Dniester River in Ukraine, and west up to and beyond the Carpathian Mountains in Romania. The inhabitants of this civilization, which lasted roughly from 5500 to 2750 BC, practiced agriculture, raised livestock, hunted, and made intricately designed pottery
+                </p>
+            </td>
+            <td>
+                <ul>
+                    <li><input type="radio" name="visibility" value=""><span>Name & Amount</span></li>
+                    <li><input type="radio" name="visibility" value=""><span>Name Only</span></li>
+                    <li><input type="radio" name="visibility" value=""><span>Anonymous</span></li>
+                </ul>
+            </td>
+         </tr>
+        <tr>
+            <td>Thu May 18.2013</td>
+            <td><span>fundrising_goal</span></td>
+            <td>10$</td>
+            <td>
+                <p>
+                    <span>Dniester River in Ukraine</span>
+                    Dniester River in Ukraine, and west up to and beyond the Carpathian Mountains in Romania. The inhabitants of this civilization, which lasted roughly from 5500 to 2750 BC, practiced agriculture, raised livestock, hunted, and made intricately designed pottery
+                </p>
+            </td>
+            <td>
+                <ul>
+                    <li><input type="radio" name="visibility" value=""><span>Name & Amount</span></li>
+                    <li><input type="radio" name="visibility" value=""><span>Name Only</span></li>
+                    <li><input type="radio" name="visibility" value=""><span>Anonymous</span></li>
+                </ul>
+            </td>
+         </tr>
+
+    </table>
+</div>
