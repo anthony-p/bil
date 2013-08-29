a4b0791da2ed1cd86f270fb56607641be7ad4d79
inna <ina_18@mail.ru>
2013-08-29 21:12:17 +0300

    contribution page

--- themes/bring_it_local/templates/mainpage_new.tpl.php
+++ themes/bring_it_local/templates/mainpage_new.tpl.php
@@ -28,6 +28,17 @@ include_once('includes/grab_video_thumbnail.php');
         $("#progressbar").progressbar({ value: 50 });
         animateProgressBar();
     });
+    $(document).ready(function(){
+       $(".description").each(function(){
+           var minHeight=72;
+           var height=$(this).height();
+           if(height <  minHeight)
+           {
+               $(this).addClass("red");
+           }
+       });
+
+    });
 </script>
 <?php /*
 <div id="main">
