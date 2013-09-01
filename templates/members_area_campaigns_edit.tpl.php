1ae7169a30db3e7cfa1cf4fe23f55497de652151
Lilian Codreanu <lilian.codreanu@gmail.com>
2013-09-01 22:30:48 +0300

    tinymce: add font size, color, family

--- templates/members_area_campaigns_edit.tpl.php
+++ templates/members_area_campaigns_edit.tpl.php
@@ -1184,21 +1184,51 @@ function clearBannerContent()
 
         tinymce.init({
             selector:'.campaign_basic',
-            plugins: ["advlist autolink lists link image charmap print preview anchor",
-            	"searchreplace visualblocks code fullscreen",
-            	"insertdatetime media table contextmenu paste moxiemanager",
-                "moxiemanager link image"
+            plugins: [
+                "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+                		"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                		"table contextmenu directionality emoticons template textcolor paste fullpage textcolor"
             ],
-            toolbar: "insertfile link image"
+            toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+            	toolbar2: "cut copy paste pastetext | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | forecolor backcolor",
+            	toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft preview",
+
+            	menubar: false,
+            	toolbar_items_size: 'small',
+
+            	style_formats: [
+            		{title: 'Bold text', inline: 'b'},
+            		{title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+            		{title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+            		{title: 'Example 1', inline: 'span', classes: 'example1'},
+            		{title: 'Example 2', inline: 'span', classes: 'example2'},
+            		{title: 'Table styles'},
+            		{title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+            	]
         });
         tinymce.init({
             selector:'.project_update_textarea',
-            plugins: ["advlist autolink lists link image charmap print preview anchor",
-            	"searchreplace visualblocks code fullscreen",
-            	"insertdatetime media table contextmenu paste moxiemanager",
-                "moxiemanager link image"
+            plugins: [
+                "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+                		"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                		"table contextmenu directionality emoticons template textcolor paste fullpage textcolor"
             ],
-            toolbar: "insertfile link image"
+            toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+            	toolbar2: "cut copy paste pastetext | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | forecolor backcolor",
+            	toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft preview",
+
+            	menubar: false,
+            	toolbar_items_size: 'small',
+
+            	style_formats: [
+            		{title: 'Bold text', inline: 'b'},
+            		{title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+            		{title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+            		{title: 'Example 1', inline: 'span', classes: 'example1'},
+            		{title: 'Example 2', inline: 'span', classes: 'example2'},
+            		{title: 'Table styles'},
+            		{title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+            	]
         });
 
     });
