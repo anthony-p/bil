966cb1c9c968d20ca91269f2479c3fa5d346f995
Dev <lilian.codreanu@gmail.com>
2013-09-02 23:36:11 +0300

    admin disable campaigns

--- admin/disable_campaigns.php
+++ admin/disable_campaigns.php
@@ -13,117 +13,140 @@ if ($session->value('adminarea')!='Active')
 }
 else
 {
-	include_once ('header.php');
+	if (!isset($_GET["user_id"]) || !$_GET["user_id"] || !is_numeric($_GET["user_id"])) {
+        include_once ('header.php');
 
-	(string) $management_box = NULL;
+        (string) $management_box = NULL;
 
-	$msg_changes_saved = '<p align="center" class="contentfont">' . AMSG_CHANGES_SAVED . '</p>';
+        $msg_changes_saved = '<p align="center" class="contentfont">' . AMSG_CHANGES_SAVED . '</p>';
 
-    $form_submitted = false;
+        $sql_select_users = $db->query("SELECT id, first_name, last_name, organization FROM bl2_users");
 
-    if (isset($_REQUEST['do']) && $_REQUEST['do'] == 'edit_user')
-    {
-        $row_user = $db->get_sql_row("SELECT npu.user_id, npu.username as npu_username, npu.active, npu.probid_user_id, u.username, npu.user_link_active,
-                                    npu.report_email, npu.report_daily, npu.report_weekly, npu.report_monthly FROM np_users npu
-	                                LEFT JOIN " . DB_PREFIX . "users u ON u.user_id = npu.probid_user_id WHERE npu.user_id=" . $_REQUEST['np_user_id']);
-
-        if ($_POST['operation'] == 'submit')
-        {
-            $form_submitted = true;
-
-            $template->set('msg_changes_saved', $msg_changes_saved);
-            $query_update_user = "UPDATE np_users SET probid_user_id='" . $_POST['users'] . "',
-                                    report_email='{$_POST["report_email"]}', report_daily='{$_POST["report_daily"]}', report_weekly='{$_POST["report_weekly"]}', report_monthly='{$_POST["report_monthly"]}',
-                                    user_link_active='" . $_POST['user_link_active'] . "' WHERE user_id=" . $_POST['np_user_id'];
-
-            $sql_update_user = $db->query($query_update_user);
+        $users = array();
+        while ($user = $db->fetch_array($sql_select_users)){
+            $users[] = $user;
         }
+        $template->set('users', $users);
 
-        if (!$form_submitted)
-        {
-            $sql_select_user = $db->query("SELECT user_id, username FROM " . DB_PREFIX . "users ORDER BY username ASC");
+        $np_users_content = '';
+        $template->set('np_users_content', $np_users_content);
 
-            $np_user_edit_content = '<select name="users">';
-            while ($user = $db->fetch_array($sql_select_user)){
-                $np_user_edit_content .= '<option value="'.$user["user_id"].'" ';
-                $np_user_edit_content .= ($row_user['probid_user_id'] == $user["user_id"]) ? 'selected' : '';
-                $np_user_edit_content .= ' >'.$user["username"].'</option>';
+
+        $template->set('header_section', AMSG_DISABLE_CAMPAIGNS);
+        $template->set('subpage_title', 'Campaigns');
+
+        $template_output .= $template->process('disable_campaigns.tpl.php');
+
+        include_once ('footer.php');
+
+        echo $template_output;
+    } else {
+        if (!isset($_GET["campaign_id"]) || !$_GET["campaign_id"] || !is_numeric($_GET["campaign_id"])) {
+            $user_id = $_GET["user_id"];
+            $campaign_type_query = '';
+            $all_selected = $draft_selected = $live_selected = $closed_selected = '';
+
+            if (isset($_GET["campaign_type"]) && $_GET["campaign_type"]) {
+                switch ($_GET["campaign_type"]) {
+                    case "draft" : $active = 0;
+                        $campaign_type_query = " AND active=0 ";
+                        $draft_selected = 'selected';
+                        break;
+                    case "live" : $active = 1;
+                        $campaign_type_query = " AND active=1 ";
+                        $live_selected = 'selected';
+                        break;
+                    case "closed" : $active = 2;
+                        $campaign_type_query = " AND active=2 ";
+                        $closed_selected = 'selected';
+                        break;
+                    default:
+                        $all_selected = 'selected';
+                }
             }
-            $np_user_edit_content .= '</select>';
-            $template->set('users', $np_user_edit_content);
-            $template->set('user_details', $row_user);
-            $template->set('do', $_REQUEST['do']);
 
-            $management_box = $template->process('list_np_users_edit_np_user.tpl.php');
+            $campaigns_select_query = $db->query(
+                "SELECT SQL_CALC_FOUND_ROWS user_id, username, email, reg_date, end_date, project_title, active, logo, banner, disabled " .
+                    " FROM np_users WHERE probid_user_id=" . $user_id . $campaign_type_query
+            );
+//        $total_number_of_rows = $db->get_sql_row("SELECT FOUND_ROWS()");
+
+            $counter = 0;
+            $np_users_content = '<tr><td></td><td></td><td></td><td></td><td><select name="campaign_type" id="campaign_type">' .
+                '<option value="all" ' . $all_selected . '>all</option>' .
+                '<option value="draft" ' . $draft_selected . '>draft</option>' .
+                '<option value="live" ' . $live_selected . '>live</option>' .
+                '<option value="closed" ' . $closed_selected . '>closed</option>' .
+                '</select></td><td></td></tr>';
+            while ($campaigns_query_result =  mysql_fetch_array($campaigns_select_query)) {
+//            $campaigns[] = $campaigns_query_result;
+                $background = ($counter++%2) ? 'c1' : 'c2';
+                $disable = $campaigns_query_result['disabled'] ? 'Enable' : 'Disable';
+                switch ($campaigns_query_result['active']) {
+                    case 0: $status = 'draft';
+                        break;
+                    case 1: $status = 'live';
+                        break;
+                    case 2: $status = 'closed';
+                        break;
+                    default: $status = '';
+                }
+
+//                if ($campaigns_query_result["end_date"] < time()) {
+//                    $status = 'closed';
+//                }
+
+//            var_dump($campaigns_query_result['user_id']);
+
+                $np_users_content .= '<tr class="' . $background . '"> '.
+                    '	<td>' . $campaigns_query_result['user_id'] . '</td> '.
+                    '	<td>' . $campaigns_query_result['project_title'] . '</td> '.
+                    '	<td>' . format_date($campaigns_query_result['reg_date']) . '</td> '.
+                    '	<td>' . format_date($campaigns_query_result['end_date']) . '</td> '.
+                    '	<td>' . $status . '</td> '.
+                    '	<td><a id="' . $campaigns_query_result['user_id'] .
+                    '" class="disable_campaign" href="javascript:void(0)">' .
+                    $disable . '</a></td> '.
+                    '	<input type="hidden" id="campaign_' . $campaigns_query_result['user_id'] .
+                    '" value="' . $campaigns_query_result['disabled'] . '"/> '.
+                    '</tr> ';
+            }
+
+//        var_dump($np_users_content); exit;
+
+            echo json_encode(array("np_users_content" => $np_users_content));
+        } else {
+            $campaign_id = $_GET["campaign_id"];
+
+            if (isset($_GET["disabled"])) {
+                if ($_GET["disabled"]) {
+                    $disabled = 0;
+                } else {
+                    $disabled = 1;
+                }
+
+                $db->query(
+                    "UPDATE np_users SET disabled=" . $disabled .
+                        " WHERE user_id=" . $campaign_id
+                );
+
+                $np_user_content = $db->get_sql_row(
+                    "SELECT user_id, username, email, reg_date, project_title, logo, banner, disabled " .
+                        " FROM np_users WHERE user_id=" . $campaign_id
+                );
+
+                $data = array(
+                    "np_user_content" => $np_user_content,
+                );
+                echo json_encode($data);
+            }
         }
     }
-
-	$template->set('management_box', $management_box);
-
-	$sql_select_users = $db->query("SELECT npu.user_id, npu.username as npu_username, npu.name, npu.active,
-	    	                                npu.email, npu.tax_company_name, npu.report_daily, npu.report_weekly, npu.report_monthly FROM np_users npu
-	    	                                LEFT JOIN " . DB_PREFIX . "users u ON u.user_id = npu.probid_user_id");
-
-    $counter = 0;
-    $np_users_content = '';
-	while ($user_details = $db->fetch_array($sql_select_users))
-	{
-//        var_dump($user_details);die;
-
-        /*
-         *           <th class="header" >np-Username</th>
-                   <th width="150" align="center">UserId</th>
-                   <th width="150" align="center">E-mail address</th>
-                   <th width="90" align="center">Contact Name</th>
-                   <th filter="false" width="150" align="center">Report selected</th>
-         *
-         * */
-		$background = ($counter++ % 2) ? 'c1' : 'c2';
-
-		$np_users_content .= '<tr class="' . $background . '"> '.
-      	'	<td>' . $user_details['user_id'] . '</td> '.
-        '	<td>' . $user_details['npu_username'] . '</td> '.
-      	'	<td>' . $user_details['email'] . '</td> '.
-      	'	<td>' . $user_details['name'] . '</td> '.
-      	'	<td align="center">' . getReportsPeriod($user_details) . '</td> '.
-      	'	<td align="center"> '.
-			'		[ <a href="list_np_users.php?do=edit_user&np_user_id=' . $user_details['user_id'] . '">' . AMSG_EDIT . '</a> ] &nbsp;'.
-			'</tr> ';
-	}
-
-	$template->set('np_users_content', $np_users_content);
-
-
-	$template->set('header_section', AMSG_USERS_MANAGEMENT);
-	$template->set('subpage_title', 'np-Users');
-
-	$template_output .= $template->process('disable_campaigns.tpl.php');
-
-	include_once ('footer.php');
-
-	echo $template_output;
 }
 
-/**
- * @param $user
- * @return string
- */
-function getReportsPeriod($user)
+function format_date($timestamp)
 {
-    $result = array();
-
-    if ($user["report_daily"] != 0)
-        $result[] = "daily";
-    if ($user["report_weekly"] != 0)
-        $result[] = "weekly";
-    if ($user["report_monthly"] != 0)
-        $result[] = "monthly";
-
-    $result = implode(" | ",$result);
-
-    if (empty($result))
-        $result = "none";
-    return $result;
+    return date("Y-m-d H:i:s", $timestamp);
 }
 
 ?>
