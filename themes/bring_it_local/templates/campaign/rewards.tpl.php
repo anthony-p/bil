4a3e2cdf9371a9c9956684b6f17645b5e61939c9
Zakaria <expertizian@gmail.com>
2013-09-03 12:09:09 +0200

    In the case a campaign doesn't contain any rewards we display the message "No rewards available" in the rewards tab in campaign front-end page.

--- themes/bring_it_local/templates/campaign/rewards.tpl.php
+++ themes/bring_it_local/templates/campaign/rewards.tpl.php
@@ -9,26 +9,30 @@ require_once (__DIR__ . '/../includes/class_project_rewards.php');
 	});
 </script>
 <aside class="announcement rewards_tab">
-	<h1>Select a reward</h1> <h2>for your contribution</h2>
-	<?php
-	foreach($projectRewards as $project_reward){
-		$available = $project_reward['available_number'] ==NULL || $project_reward['available_number'] > $project_reward['given_number'];
-	?>
-		<div class="reward_display">
-			<div class="reward_amount">$<?= $project_reward['amount']; ?></div>
-			<div class="reward_name"><?= $project_reward['name']; ?></div>
-			<div class="reward_description"><?= $project_reward['description']; ?></div>
-			<div class="reward_bottom">
-				<div class="reward_claimed_number"><?= $project_reward['given_number']; ?> claimed</div>
-	<?php if($available) : ?>
-				<div class="donate_now_button">
-					<span class="uper">Donate Now</span>
-					<span>make a donation</span>
+	<?php if(count($projectRewards) > 0){ ?>
+		<h1>Select a reward</h1> <h2>for your contribution</h2>
+		<?php
+		foreach($projectRewards as $project_reward){
+			$available = $project_reward['available_number'] ==NULL || $project_reward['available_number'] > $project_reward['given_number'];
+		?>
+			<div class="reward_display">
+				<div class="reward_amount">$<?= $project_reward['amount']; ?></div>
+				<div class="reward_name"><?= $project_reward['name']; ?></div>
+				<div class="reward_description"><?= $project_reward['description']; ?></div>
+				<div class="reward_bottom">
+					<div class="reward_claimed_number"><?= $project_reward['given_number']; ?> claimed</div>
+		<?php if($available) : ?>
+					<div class="donate_now_button">
+						<span class="uper">Donate Now</span>
+						<span>make a donation</span>
+					</div>
+		<?php endif; ?>
 				</div>
-	<?php endif; ?>
 			</div>
-		</div>
-	<?php
-	}
-	?>
+		<?php
+		}
+		?>
+	<?php } else {?>
+		<h1 class="empty_rewards_message">No rewards are available</h1>
+	<?php }?>
 </aside>
