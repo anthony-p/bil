81cc0452f018445740f42a800f2a662854beee86
Zakaria <expertizian@gmail.com>
2013-09-03 07:53:24 +0200

    In campaign details frontend page: adding the number of claimed rewards and donate button, at this point donate button is not yet functional.

--- themes/bring_it_local/templates/campaign/rewards.tpl.php
+++ themes/bring_it_local/templates/campaign/rewards.tpl.php
@@ -1,7 +1,13 @@
 <?php
 require_once (__DIR__ . '/../includes/class_project_rewards.php');
 ?>
-
+<script>
+    $(document).ready(function(){
+		$(".donate_now_button").click(function(){
+			alert("This feature has not been implemented yet.");
+		});
+	});
+</script>
 <aside class="announcement rewards_tab">
 	<h1>Select a reward</h1> <h2>for your contribution</h2>
 	<?php
@@ -11,6 +17,13 @@ require_once (__DIR__ . '/../includes/class_project_rewards.php');
 			<div class="reward_amount">$<?= $project_reward['amount']; ?></div>
 			<div class="reward_name"><?= $project_reward['name']; ?></div>
 			<div class="reward_description"><?= $project_reward['description']; ?></div>
+			<div class="reward_bottom">
+				<div class="reward_claimed_number"><?= $project_reward['given_number']; ?> claimed</div>
+				<div class="donate_now_button">
+					<span class="uper">Donate Now</span>
+					<span>make a donation</span>
+				</div>
+			</div>
 		</div>
 	<?php
 	}
