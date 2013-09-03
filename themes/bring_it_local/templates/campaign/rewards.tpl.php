9e42be3918272e3337a3a9db43d4477689191829
Zakaria <expertizian@gmail.com>
2013-09-03 11:53:06 +0200

    In rewards tab in campaign front end page: the "donate now" button must not be visible if the available number of rewards has been reached.

--- themes/bring_it_local/templates/campaign/rewards.tpl.php
+++ themes/bring_it_local/templates/campaign/rewards.tpl.php
@@ -12,6 +12,7 @@ require_once (__DIR__ . '/../includes/class_project_rewards.php');
 	<h1>Select a reward</h1> <h2>for your contribution</h2>
 	<?php
 	foreach($projectRewards as $project_reward){
+		$available = $project_reward['available_number'] ==NULL || $project_reward['available_number'] > $project_reward['given_number'];
 	?>
 		<div class="reward_display">
 			<div class="reward_amount">$<?= $project_reward['amount']; ?></div>
@@ -19,10 +20,12 @@ require_once (__DIR__ . '/../includes/class_project_rewards.php');
 			<div class="reward_description"><?= $project_reward['description']; ?></div>
 			<div class="reward_bottom">
 				<div class="reward_claimed_number"><?= $project_reward['given_number']; ?> claimed</div>
+	<?php if($available) : ?>
 				<div class="donate_now_button">
 					<span class="uper">Donate Now</span>
 					<span>make a donation</span>
 				</div>
+	<?php endif; ?>
 			</div>
 		</div>
 	<?php
