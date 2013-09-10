c70a41c17b350c0e744fa4b3d9699501679c3bb6
Dev <lilian.codreanu@gmail.com>
2013-09-10 23:47:29 +0300

    vote us email

--- vote_us.php
+++ vote_us.php
@@ -26,32 +26,32 @@ if ($session->value('user_id') && $campaign_id)
         $user_voted_email = $projectVotes->getUserEmail();
         $campaign_owner_email = $projectVotes->getCampaignOwnerEmail();
 
-        if (!defined('INCLUDED')) {
-            define('INCLUDED', 1);
-        }
-        include_once ('language/english/mails/voting_campaign_owner_notification.php');
-        include_once ('language/english/mails/voting_user_notification.php');
+//        if (!defined('INCLUDED')) {
+//            define('INCLUDED', 1);
+//        }
+//        include_once ('language/english/mails/voting_campaign_owner_notification.php');
+//        include_once ('language/english/mails/voting_user_notification.php');
 
-//        $subject = "Campaign voting";
-//        $html_message_user = 'Thank you for voting for ' . $campaign_title . ' campaign!';
-//        $html_message_owner = 'Your campaign, ' . $campaign_title . ', received a vote!';
-//
-//        $headers = 'From: Bring It Local <support@bringitlocal.com>' . PHP_EOL .
-//            'X-Mailer: PHP-' . phpversion() . PHP_EOL .
-//            'Content-type: text/html; charset=iso-8859-1' . PHP_EOL;
-//
-//        $uid = md5(uniqid(time()));
-//        $header = "From: Bring It Local <support@bringitlocal.com> \r\n";
-//        $header .= "MIME-Version: 1.0\r\n";
-//        $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-//        $header .= "This is a multi-part message in MIME format.\r\n";
-//        $header .= "--".$uid."\r\n";
-//        $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-//        $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-//        $header .= "--".$uid."--";
-//
-//        mail($user_voted_email, $subject, $html_message_user, $header) ;
-//        mail($campaign_owner_email, $subject, $html_message_owner, $header) ;
+        $subject = "Campaign voting";
+        $html_message_user = 'Thank you for voting for ' . $campaign_title . ' campaign!';
+        $html_message_owner = 'Your campaign, ' . $campaign_title . ', received a vote!';
+
+        $headers = 'From: Bring It Local <support@bringitlocal.com>' . PHP_EOL .
+            'X-Mailer: PHP-' . phpversion() . PHP_EOL .
+            'Content-type: text/html; charset=iso-8859-1' . PHP_EOL;
+
+        $uid = md5(uniqid(time()));
+        $header = "From: Bring It Local <support@bringitlocal.com> \r\n";
+        $header .= "MIME-Version: 1.0\r\n";
+        $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+        $header .= "This is a multi-part message in MIME format.\r\n";
+        $header .= "--".$uid."\r\n";
+        $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+        $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+        $header .= "--".$uid."--";
+
+        mail($user_voted_email, $subject, $html_message_user, $header) ;
+        mail($campaign_owner_email, $subject, $html_message_owner, $header) ;
     }
 }
 
