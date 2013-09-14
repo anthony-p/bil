65b9a85a3c3c42a069aa28e6d6a90a57ff5ec0b1
inna <ina_18@mail.ru>
2013-09-14 23:33:19 +0300

    contribution page

--- templates/about_me.tpl.php
+++ templates/about_me.tpl.php
@@ -9,47 +9,49 @@
 if ( !defined('INCLUDED') ) { die("Access Denied"); }
 ?>
 <link href="/css/tinyeditor.css" rel="stylesheet">
-<br>nbnbnb
+<br>
 <div class="about-me_block">
     <div id="avatar_about_me">
         <?php if(!empty($user_details['avatar'])) :?>
             <div class="upload_logo"><img src="<?=$user_details['avatar']?>"/></div>
         <?php endif;?>
     </div>
-    <div>
-        <p>
-            <?php echo isset ($user_details["first_name"]) ? $user_details["first_name"] : '' ?> <?php echo isset ($user_details["last_name"]) ? $user_details["last_name"] : '' ?>
-        </p>
-    </div>
-    <div id="about_me_content">
+    <div class="about_me_content">
         <div>
             <p>
-                Location:
-                <?php echo isset ($user_details["city"]) ? $user_details["city"] : '' ?> <?php echo isset ($user_details["address"]) ? $user_details["address"] : '' ?>
+                <?php echo isset ($user_details["first_name"]) ? $user_details["first_name"] : '' ?> <?php echo isset ($user_details["last_name"]) ? $user_details["last_name"] : '' ?>
             </p>
         </div>
-        <div>
-           <?=MSG_ALSO_FIND_ME?>
-            <?php if (!empty($user_details['facebook_link'])) :?>
-                <label class="facebook">
-                    <a href="<?=$user_details['facebook_link']?>" target="_blank">Facebook</a>
-                </label>
-            <?php endif;?>
-            <?php if (!empty($user_details['twitter_link'])) :?>
-                <label class="twitter">
-                    <a href="<?=$user_details['twitter_link']?>" target="_blank">Twitter</a>
-                </label>
-            <?php endif;?>
-            <?php if (!empty($user_details['google_link'])) :?>
-                <label class="google">
-                    <a href="<?=$user_details['google_link']?>" target="_blank">Google+</a>
-                </label>
-            <?php endif;?>
-        </div>
-        <div>
-            <?php if (!empty($user_details['about_me'])) :?>
-                <?=html_entity_decode($user_details['about_me'])?>
-            <?php endif; ?>
+        <div id="about_me_content">
+            <div>
+                <p>
+                    Location:
+                    <?php echo isset ($user_details["city"]) ? $user_details["city"] : '' ?> <?php echo isset ($user_details["address"]) ? $user_details["address"] : '' ?>
+                </p>
+            </div>
+            <div>
+                <?=MSG_ALSO_FIND_ME?>
+                <?php if (!empty($user_details['facebook_link'])) :?>
+                    <label class="facebook">
+                        <a href="<?=$user_details['facebook_link']?>" target="_blank" class="facebook"></a>
+                    </label>
+                <?php endif;?>
+                <?php if (!empty($user_details['twitter_link'])) :?>
+                    <label class="twitter">
+                        <a href="<?=$user_details['twitter_link']?>" target="_blank" class="twitter"></a>
+                    </label>
+                <?php endif;?>
+                <?php if (!empty($user_details['google_link'])) :?>
+                    <label class="google">
+                        <a href="<?=$user_details['google_link']?>" target="_blank">Google+</a>
+                    </label>
+                <?php endif;?>
+            </div>
+            <div>
+                <?php if (!empty($user_details['about_me'])) :?>
+                    <?=html_entity_decode($user_details['about_me'])?>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </div>
