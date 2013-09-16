322bbc1281bfa858d7c32c2d0deb1eafcba0a6f7
Dev <lilian.codreanu@gmail.com>
2013-09-16 11:38:57 +0300

    payment notification

--- donate_notify_url.php
+++ donate_notify_url.php
@@ -28,49 +28,49 @@ $user_id = 0;
 if (!$user_id) {
     if (isset($_POST) && isset($_POST['payer_email']) && $_POST['payer_email']) {
         $payer_email = $_POST['payer_email'];
-        if ($_POST['payer_email'] == 'buyer@paypalsandbox.com') {
-            $payer_email = 'rlpc.test@gmail.com';
-            $db->query(
-                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                    10 . ", " . 10 . ", " . 10 . ", " . time() . ")"
-            );
-        }
-        $db->query(
-            "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                11 . ", " . 11 . ", " . 11 . ", " . time() . ")"
-        );
+//        if ($_POST['payer_email'] == 'buyer@paypalsandbox.com') {
+//            $payer_email = 'rlpc.test@gmail.com';
+//            $db->query(
+//                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                    10 . ", " . 10 . ", " . 10 . ", " . time() . ")"
+//            );
+//        }
+//        $db->query(
+//            "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                11 . ", " . 11 . ", " . 11 . ", " . time() . ")"
+//        );
         $user_id = $db->get_sql_field("SELECT id FROM bl2_users WHERE
 				pg_paypal_email='" . $payer_email . "'", 'id');
 
-        $db->query(
-            "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                12 . ", " . 12 . ", " . 12 . ", " . time() . ")"
-        );
+//        $db->query(
+//            "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                12 . ", " . 12 . ", " . 12 . ", " . time() . ")"
+//        );
 
         if ($user_id) {
             $_SESSION["probid_user_id"] = $user_data['id'];
-            $db->query(
-                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                    $user_id . ", " . 1 . ", " . 1 . ", " . time() . ")"
-            );
-            $db->query(
-                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                    $user_id . ", " . 2 . ", " . 2 . ", " . time() . ")"
-            );
-            $db->query(
-                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                    $user_id . ", " . 3 . ", " . 3 . ", " . time() . ")"
-            );
-            $db->query(
-                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                    $user_id . ", " . 4 . ", " . 4 . ", " . time() . ")"
-            );
+//            $db->query(
+//                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                    $user_id . ", " . 1 . ", " . 1 . ", " . time() . ")"
+//            );
+//            $db->query(
+//                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                    $user_id . ", " . 2 . ", " . 2 . ", " . time() . ")"
+//            );
+//            $db->query(
+//                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                    $user_id . ", " . 3 . ", " . 3 . ", " . time() . ")"
+//            );
+//            $db->query(
+//                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                    $user_id . ", " . 4 . ", " . 4 . ", " . time() . ")"
+//            );
         } else {
 
-            $db->query(
-                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
-                    13 . ", " . 13 . ", " . 13 . ", " . time() . ")"
-            );
+//            $db->query(
+//                "INSERT INTO funders (user_id, campaign_id, amount, created_at) VALUES (" .
+//                    13 . ", " . 13 . ", " . 13 . ", " . time() . ")"
+//            );
         }
     }
 }
