dd623df58b85bf4311ce2c727b7cabbfc67e3bc4
Dev <lilian.codreanu@gmail.com>
2013-09-17 21:28:40 +0300

    empty email messages

--- language/english/mails/voting_campaign_owner_notification.php
+++ language/english/mails/voting_campaign_owner_notification.php
@@ -10,18 +10,7 @@ if ( !defined('INCLUDED') ) { die("Access Denied"); }
 $subject = "Your campaign received a vote!";
 $html_message_owner = 'Your campaign, ' . $campaign_title . ', received a vote!';
 
-$headers = 'From: Bring It Local <support@bringitlocal.com>' . PHP_EOL .
-    'X-Mailer: PHP-' . phpversion() . PHP_EOL .
-    'Content-type: text/html; charset=iso-8859-1' . PHP_EOL;
+$header = "From: Bring It Local <support@bringitlocal.com>";
 
-$uid = md5(uniqid(time()));
-$header = "From: Bring It Local <support@bringitlocal.com> \r\n";
-$header .= "MIME-Version: 1.0\r\n";
-$header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-$header .= "This is a multi-part message in MIME format.\r\n";
-$header .= "--".$uid."\r\n";
-$header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-$header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-$header .= "--".$uid."--";
-
-mail($campaign_owner_email, $subject, $html_message_owner, $header) ;
+mail($campaign_owner_email, $subject, $html_message_owner) ;
+//mail($campaign_owner_email, $subject, $html_message_owner, $header) ;
--- language/english/mails/voting_user_notification.php
+++ language/english/mails/voting_user_notification.php
@@ -10,18 +10,7 @@ if ( !defined('INCLUDED') ) { die("Access Denied"); }
 $subject = "Thanks for voting";
 $html_message_user = 'Thank you for voting for ' . $campaign_title . ' campaign!';
 
-$headers = 'From: Bring It Local <support@bringitlocal.com>' . PHP_EOL .
-    'X-Mailer: PHP-' . phpversion() . PHP_EOL .
-    'Content-type: text/html; charset=iso-8859-1' . PHP_EOL;
+$header = "From: Bring It Local <support@bringitlocal.com>";
 
-$uid = md5(uniqid(time()));
-$header = "From: Bring It Local <support@bringitlocal.com> \r\n";
-$header .= "MIME-Version: 1.0\r\n";
-$header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-$header .= "This is a multi-part message in MIME format.\r\n";
-$header .= "--".$uid."\r\n";
-$header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-$header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-$header .= "--".$uid."--";
-
-mail($user_voted_email, $subject, $html_message_user, $header) ;
+mail($user_voted_email, $subject, $html_message_user) ;
+//mail($user_voted_email, $subject, $html_message_user, $header) ;
