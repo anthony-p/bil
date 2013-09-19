f2f8d21d8eefcf951411ddd48631156a77605d8b
Dev <lilian.codreanu@gmail.com>
2013-09-19 21:03:00 +0300

    about me bug fixing

--- database/structure.sql
+++ database/structure.sql
@@ -3688,7 +3688,7 @@ ALTER TABLE `np_users` ADD COLUMN `cfc` INT NULL DEFAULT 0  AFTER `confirmed_pay
 
 ALTER TABLE `bl2_users` ADD COLUMN `cfc_donated` INT NULL DEFAULT 0  AFTER `confirmed_paypal_email` ;
 
-ALTER TABLE `np_users` ADD COLUMN `pattern_id` INT NOT NULL AFTER `cfc` ;
+ALTER TABLE `np_users` ADD COLUMN `parrent_id` INT NOT NULL AFTER `cfc` ;
 
 ALTER TABLE `np_users` ADD COLUMN `clone_campaign` INT NOT NULL DEFAULT 0  AFTER `active` ;
 
--- members_area.php
+++ members_area.php
@@ -5179,9 +5179,8 @@ else
     {
         if ($section == 'main')
         {
-            var_dump("SELECT np_users.project_title FROM np_users WHERE np_users.parrent_id<>0 AND np_users.probid_user_id=" . $session->value('user_id')."");
             $campaigns_result = $db->query("SELECT np_users.project_title FROM np_users WHERE np_users.parrent_id<>0 AND np_users.probid_user_id=" . $session->value('user_id')."");
-            var_dump($campaigns_result);
+
             $nrElement = mysql_num_rows($campaigns_result);
 
             $per_page = 10;
