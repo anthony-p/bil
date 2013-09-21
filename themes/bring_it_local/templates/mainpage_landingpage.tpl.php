8c1b8bd584dc8696c4f61ce1af49b5a19151a12e
Zakaria <expertizian@gmail.com>
2013-09-22 00:50:23 +0200

    In campaign frontend page: if the logged in user owns the campaign, we hide the donate button that is displayed in the right sidebar.

--- themes/bring_it_local/templates/mainpage_landingpage.tpl.php
+++ themes/bring_it_local/templates/mainpage_landingpage.tpl.php
@@ -150,7 +150,7 @@ $featured_columns = 14;
         </div>
         <div class="navigation-btn">
             <h3><?=MSG_MANY_WAYS_TO_GIVE?></h3>
-            <?php if ($compaigns['active'] != 2 && ($compaigns['end_date']-time())>0 ): ?>
+            <?php if ($compaigns['probid_user_id'] != $session->value('user_id') && $compaigns['active'] != 2 && ($compaigns['end_date']-time())>0): ?>
                 <a href="donate.php?np_userid=<?php echo isset($compaigns['user_id']) ? $compaigns['user_id'] : '0'; ?>"
                    class="donation campaign_donation">
                     <span class="uper"><?=MSG_DONATE_NOW?></span>
