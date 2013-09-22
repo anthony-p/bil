0b535f2276c06e19863601f1abfc1ed7d8bc8383
Mark White <mwhite@bringitlocal.com>
2013-09-22 10:34:33 -0700

    swap the help messages on the status screen so they match correctly with the item they refer to clone campaign and extend campaign

--- language/english/site.lang.php
+++ language/english/site.lang.php
@@ -9,8 +9,9 @@
 define('MSG_REGISTER_SUCCESS_TYPE3', '<br>Your new campaign has been saved!<br><br> You can find it and edit it <a href="/campaigns,page,drafts,section,members_area">here</a>');
 
 define('MSG_MEMBER_AREA_LIVE_STATUS_TOOLTIP', 'Set your campaign status by choosing one of these options. Choosing Live will immediately make your campaign public.');
-define('MSG_MEMBER_AREA_CLONE_CAMPAIGN_TOOLTIP', 'Choose to extend your existing campaign for a fixed number of days.');
-define('MSG_MEMBER_AREA_EXTENDS_DATE_EXISTING_CAMPAIGN_TOOLTIP', 'Choose to automatically publish an exact clone of this campaign when it closes. It will not have any comments, updates or contributions.');
+
+define('MSG_MEMBER_AREA_EXTENDS_DATE_EXISTING_CAMPAIGN_TOOLTIP', 'Choose to extend your existing campaign for a fixed number of days.');
+define('MSG_MEMBER_AREA_CLONE_CAMPAIGN_TOOLTIP', 'Choose to automatically publish an exact clone of this campaign when it closes. It will not have any comments, updates or contributions.');
 
 //member area - message dialog
 define('MSG_MEMBER_AREA_DIALOG_DELETE_CAMPAIGN_TITLE', 'Delete Campaign');
