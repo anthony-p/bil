6cc556f48efd2c45a5ecef8073dba4c9c805bed7
Mark White <mwhite@bringitlocal.com>
2013-10-09 07:07:31 -0700

    edited paypall tooltip in members area>account details

--- language/english/site.lang.php
+++ language/english/site.lang.php
@@ -171,7 +171,7 @@ define('MSG_MEMBERS_AREA_CONTRIBUTIONS_TBL_VISIBILITY', 'Visibility');
 define('MSG_MEMBERS_AREA_CONTRIBUTIONS_TBL_PP_STATUS', 'paypal Status');
 
 // tooltips
-define('TOOLTIP_REGISTRATION_DIRECT_PAYMENT_EXPLAIN', 'You can fill these in at any time. All 3 must be entered and they must match your paypal account.');
+define('TOOLTIP_REGISTRATION_DIRECT_PAYMENT_EXPLAIN', 'You can fill these in at any time. All 3 must be entered and they must match your PayPal account.');
 
 //additions
 define ('MSG_MY_EARNINGS_SUMMARY', 'Summary Report');
