7c84e6299e592a69e32092c6959fe57ad3019db3
dev <r.pavelco@gmail.com>
2013-10-15 23:22:54 +0300

    Vote button does not reappear in the new month

--- includes/class_project_votes.php
+++ includes/class_project_votes.php
@@ -37,7 +37,7 @@ class projectVotes extends custom_field
 
     function checkDonated()
     {
-        return true;
+//        return true;
         if ($this->user_id) {
             return $this->getField("SELECT count(*) FROM funders WHERE user_id=".$this->user_id." and MONTH(FROM_UNIXTIME(created_at)) = MONTH(NOW()) and YEAR(FROM_UNIXTIME(created_at)) = YEAR(NOW())");
         }
@@ -52,7 +52,7 @@ class projectVotes extends custom_field
         $compare_date = time() - (3600 * 24 * 30);
         if ($this->user_id && $this->campaign_id) {
             $voted = $this->getField("SELECT count(*) FROM project_votes WHERE user_id=" .
-                $this->user_id . " AND date>" . $compare_date);
+                $this->user_id . " AND MONTH(FROM_UNIXTIME(date)) = MONTH(NOW()) AND YEAR(FROM_UNIXTIME(date)) = YEAR(NOW())");
             if ($voted) {
                 return true;
             }
