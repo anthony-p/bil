37dde3ffcbd49e03231603352c19cab68dee7124
Anastasia Batieva <batieva.al@gmail.com>
2013-10-15 16:13:55 -0500

    Simplified tinymce buttons

--- scripts/init_tinymce.js
+++ scripts/init_tinymce.js
@@ -14,11 +14,16 @@ function init_tinymce(selector) {
             "wordcount insertdatetime media nonbreaking ",
             "moxiemanager"
         ],
-        toolbar1: "formatselect | bold italic underline | alignleft | bullist numlist | link unlink image media | removeformat | preview",
+        toolbar1: "styleselect | bold italic underline | alignleft | bullist numlist | link unlink image media | removeformat | preview",
 
         menubar: false,
         image_advtab: true,
-        toolbar_items_size: 'small'
+        toolbar_items_size: 'small',
+        style_formats: [
+            {title: 'Paragraph', block: 'p'},
+            {title: 'Header 1', block: 'span', styles: {color: '#444', font: "1.5em OpenSans, sans-serif", margin: '0 0 1em 0'}},
+            {title: 'Header 2', block: 'span', styles: {color: '#444', font: "bold 1.3em OpenSans, sans-serif", margin: '0 0 1em 0'}}
 
+        ]
     });
 }
