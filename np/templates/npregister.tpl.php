ff644ab6e860a49e6a94b2a4c573fde6646b075f
Anthony <anthony.puggioni2@gmail.com>
2013-10-17 10:57:45 +0200

    tooltip for paypal

--- np/templates/npregister.tpl.php
+++ np/templates/npregister.tpl.php
@@ -248,17 +248,21 @@ if ( !defined('INCLUDED') ) { die("Access Denied"); }
     </div>
     
         <div class="account-row">
-            <label><? echo MSG_PG_PAYPAL_EMAIL_ADDRESS; ?> *</label>
+            <label>
+            <? echo MSG_PG_PAYPAL_EMAIL_ADDRESS; ?> 
+            <img src="/images/question_help.png" height="16" alt="help"  title="<?= TOOLTIP_REGISTRATION_DIRECT_PAYMENT_EXPLAIN ?>" style="margin-left: 10px;">
+            </label>
             <input name="pg_paypal_email" type="text" id="pg_paypal_email" value="<?=(isset($user_details['pg_paypal_email']))?$user_details['pg_paypal_email']:'';?>" size="40"/>
+            
             <span><?= MSG_PG_PAYPAL_EMAIL_ADDRESS_EXPL; ?></span>
         </div>
         <div class="account-row">
-            <label><?= MSG_PG_PAYPAL_FIRST_NAME; ?> *</label>
+            <label><?= MSG_PG_PAYPAL_FIRST_NAME; ?> </label>
             <input name="pg_paypal_first_name" type="text" id="pg_paypal_first_name" value="<?=(isset($user_details['pg_paypal_first_name']))?$user_details['pg_paypal_first_name']:'';?>" size="40"/>
             <span><?= MSG_PG_PAYPAL_FIRST_NAME_EXPL; ?></span>
         </div>
         <div class="account-row">
-            <label><?= MSG_PG_PAYPAL_LAST_NAME; ?> *</label>
+            <label><?= MSG_PG_PAYPAL_LAST_NAME; ?> </label>
             <input name="pg_paypal_last_name" type="text" id="pg_paypal_last_name" value="<?=(isset($user_details['pg_paypal_last_name']))?$user_details['pg_paypal_last_name']:'';?>" size="40"/>
             <span><?= MSG_PG_PAYPAL_LAST_NAME_EXPL; ?></span>
         </div>
