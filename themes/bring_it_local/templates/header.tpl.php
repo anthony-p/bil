1b6055411266aa1b3b692488a671f9d9ba1a78d8
Anthony <anthony.puggioni2@gmail.com>
2013-11-01 07:31:42 +0100

    aaa fixed

--- themes/bring_it_local/templates/header.tpl.php
+++ themes/bring_it_local/templates/header.tpl.php
@@ -16,7 +16,7 @@ global $coupon_url;
 <html>
 <head>
     <?php if(!isset($page_specific_title)) $page_specific_title =''; ?>
-    <title><?=$page_title. $page_specific_title;?> aaa</title>
+    <title><?=$page_title. $page_specific_title;?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=<?=LANG_CODEPAGE;?>">
     <meta http-equiv=”X-UA-Compatible” content=”IE=9″ />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
