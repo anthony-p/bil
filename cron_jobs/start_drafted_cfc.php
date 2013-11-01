9760acfd666abbf5305973bf6a80f6c48e66d645
Alexey Dudich <dudich.dev@gmail.com>
2013-11-01 15:38:00 +0200

    renew cfc cron job fix

--- cron_jobs/start_drafted_cfc.php
+++ cron_jobs/start_drafted_cfc.php
@@ -4,7 +4,7 @@ session_start();
 include_once ('../includes/global.php');
 
 $today     = strtotime('today');
-$campaigns = $db->query("SELECT * FROM np_user WHERE active = 0 AND cfc = 1 AND start_date = $today");
+$campaigns = $db->query("SELECT * FROM np_users WHERE active = 0 AND cfc = 1 AND start_date = $today");
 
 while ($query_result = mysql_fetch_assoc($campaigns)) {
 
@@ -13,6 +13,6 @@ while ($query_result = mysql_fetch_assoc($campaigns)) {
 }
 foreach ($starting_campaigns as $starting_campaign) {
 
-	$db->query("UPDATE np_users SET active = 1 WHERE user_id = $starting_campaign['user_id']");
+	$db->query("UPDATE np_users SET active = 1 WHERE user_id = " . $starting_campaign['user_id']);
 	
 }
