615271ded641c719a47fb2c1c45762e1bd1a28f3
Anthony <anthony.puggioni2@gmail.com>
2013-11-07 17:34:26 +0100

    vote us

--- language/english/mails/voting_campaign_owner_notification.php
+++ language/english/mails/voting_campaign_owner_notification.php
@@ -5,16 +5,35 @@
  * Date: 9/10/13
  * Time: 11:33 PM
  */
-if ( !defined('INCLUDED') ) { die("Access Denied"); }
+if (!defined('INCLUDED')) { die("Access Denied");
+}
 
-$subject = "Your campaign received a vote!";
-$html_message_owner = 'Your campaign,'. $campaign_title .',received a vote! <br>Active site users are able to vote for their favorite campaigns. The Community Fund will be disbursed each
-month to the campaign that receives the most votes. You can read more about the Community Fund and Bring It Local\'s experiment in democracy here: <a href="http://dev2.bringitlocal.com/bringitlocal">Community Fund</a>';
+$send = true;
+// always sent;
+
+## text message - editable
+$text_message_owner = 'Your campaign, %1$s ,received a vote! 
+Active site users are able to vote for their favorite campaigns. 
+The Community Fund will be disbursed each month to the campaign that receives the most votes. 
+You can read more about the Community Fund and 
+Bring It Local\'s experiment in democracy here: <a href="http://dev2.bringitlocal.com/bringitlocal">Community Fund</a>';
+
+$html_message_owner = 'Your campaign, %1$s ,received a vote! <br>
+Active site users are able to vote for their favorite campaigns. <br>
+The Community Fund will be disbursed each month to the campaign that receives the most votes. <br>  
+You can read more about the Community Fund and <br>
+Bring It Local\'s experiment in democracy here: <a href="http://dev2.bringitlocal.com/bringitlocal">Community Fund</a>';
+
+$text_message_owner = sprintf($text_message_owner, $campaign_title);
+$html_message_owner = sprintf($html_message_owner, $campaign_title);
+
+$text_message_owner = str_replace('+', '', $text_message_owner);
 $html_message_owner = str_replace('+', '', $html_message_owner);
 
-$headers = 'From: Bring It Local <support@bringitlocal.com>' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$subject = "Your campaign received a vote!";
+
+//$headers = 'From: Bring It Local <support@bringitlocal.com>' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 
 //mail($campaign_owner_email, $subject, $html_message_owner, $headers) ;
-send_mail($campaign_owner_email, $subject, $html_message_owner, 
-    $this->setts['admin_email'], $html_message_owner, null, true);
+send_mail($campaign_owner_email, $subject, $text_message_owner, $this -> setts['admin_email'], $html_message_owner, null, true);
+?>
--- language/english/mails/voting_user_notification.php
+++ language/english/mails/voting_user_notification.php
@@ -5,15 +5,24 @@
  * Date: 9/10/13
  * Time: 11:32 PM
  */
-if ( !defined('INCLUDED') ) { die("Access Denied"); }
+if (!defined('INCLUDED')) { die("Access Denied");
+}
 
-$subject = "Thanks for voting";
-$html_message_user = 'Thank you for voting for ' . $campaign_title . ' campaign!';
+$send = true;
+// always sent;
+
+$text_message_user = 'Thank you for voting for %1$s campaign!';
+$html_message_user = 'Thank you for voting for %1$s campaign!';
+
+$text_message_user = sprintf($text_message_user, $campaign_title);
+$html_message_user = sprintf($html_message_user, $campaign_title);
+
+$text_message_user = str_replace('+', '', $html_message_user);
 $html_message_user = str_replace('+', '', $html_message_user);
 
-$headers = 'From: Bring It Local <support@bringitlocal.com>' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$subject = "Thanks for voting";
 
+//$headers = 'From: Bring It Local <support@bringitlocal.com>' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 //mail($user_voted_email, $subject, $html_message_user, $headers) ;
-send_mail($user_voted_email, $subject, $html_message_user, 
-    $setts['admin_email'], $html_message_user, null, true);
+send_mail($user_voted_email, $subject, $text_message_user, $setts['admin_email'], $html_message_user, null, true);
+?>
