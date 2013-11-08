412bb417fd18058b90c7e252a01d06823339b470
Mark White <mwhite@bringitlocal.com>
2013-11-08 12:24:03 -0800

    edits to the text of the vote notification emails

--- language/english/mails/voting_campaign_owner_notification.php
+++ language/english/mails/voting_campaign_owner_notification.php
@@ -12,16 +12,22 @@ $send = true;
 // always sent;
 
 ## text message - editable
-$text_message_owner = 'Your campaign, %1$s ,received a vote! 
-Active site users are able to vote for their favorite campaigns. 
-The Community Fund will be disbursed each month to the campaign that receives the most votes. 
-You can read more about the Community Fund and 
-Bring It Local\'s experiment in democracy here: <a href="http://dev2.bringitlocal.com/bringitlocal">Community Fund</a>';
+$text_message_owner = 'Congratulations! Your campaign, %1$s, just received a vote! This makes it eligible to receive this month\'s funds from the Bring It Local Community Fund.
+How does this work?
 
-$html_message_owner = 'Your campaign, %1$s ,received a vote! <br>
-Active site users are able to vote for their favorite campaigns. <br>
-The Community Fund will be disbursed each month to the campaign that receives the most votes. <br>  
-You can read more about the Community Fund and <br>
+Anyone who makes some form of contribution on the site to any campaign gains the right to vote for their favorite campaign. 
+Then, at the end of each month the Community Fund will be disbursed to the campaign that has received the most votes. 
+
+Read more about the Community Fund and Bring It Local\'s experiment in democracy here: <a href="http://dev2.bringitlocal.com/bringitlocal">Community Fund</a>';
+
+$html_message_owner = 'Congratulations! Your campaign, %1$s, just received a vote! <br>
+This makes it eligible to receive this month\'s funds from the Bring It Local Community Fund.<br><br>
+<strong>How does this work?</strong><br>
+Anyone who makes some form of contribution on the site to any campaign gains the right to vote for their favorite campaign. <br>
+Then, at the end of each month the Community Fund will be disbursed to the campaign that has received the most votes. <br> <br>
+
+<strong>Read more </strong><br>
+Read more about the Community Fund and see all the details about past history and the current vote report as well as all other details about 
 Bring It Local\'s experiment in democracy here: <a href="http://dev2.bringitlocal.com/bringitlocal">Community Fund</a>';
 
 $text_message_owner = sprintf($text_message_owner, $campaign_title);
@@ -30,7 +36,7 @@ $html_message_owner = sprintf($html_message_owner, $campaign_title);
 $text_message_owner = str_replace('+', '', $text_message_owner);
 $html_message_owner = str_replace('+', '', $html_message_owner);
 
-$subject = "Your campaign received a vote!";
+$subject = "Congratulations! Your Bring It Local crowdfunding campaign just received a vote!";
 
 //$headers = 'From: Bring It Local <support@bringitlocal.com>' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 
