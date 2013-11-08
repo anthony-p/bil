fd55ea42ae5f1c7ac8e9e9c19d0dd75b8abf2d97
Anthony <anthony.puggioni2@gmail.com>
2013-11-08 09:15:45 +0100

    vote us testing

--- vote_us.php
+++ vote_us.php
@@ -17,11 +17,13 @@ $campaign_title = isset($_GET["campaign_title"]) ? $_GET["campaign_title"] : '';
 if ($session -> value('user_id') && $campaign_id) {
 	require_once (dirname(__FILE__) . '/includes/class_project_votes.php');
 	$projectVotes = new projectVotes($session -> value('user_id'), $campaign_id);
-	$vote_us = $projectVotes -> vote();
+	//$vote_us = $projectVotes -> vote();
 	//if (isset($vote_us["success"]) && $vote_us["success"]) {
-		$user_voted_email = $projectVotes -> getUserEmail();
-		$campaign_owner_email = $projectVotes -> getCampaignOwnerEmail();
-
+		//$user_voted_email = $projectVotes -> getUserEmail();
+		//$campaign_owner_email = $projectVotes -> getCampaignOwnerEmail();
+		$user_voted_email = "anthony.puggioni2@gmail.com";
+		$campaign_owner_email = "anthony.puggioni2@gmail.com";
+		
 		if (!defined('INCLUDED')) {
 			define('INCLUDED', 1);
 		}
