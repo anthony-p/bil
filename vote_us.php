3976d79273fea7a905b2a84660c2a00ac14281d5
Anthony <anthony.puggioni2@gmail.com>
2013-11-08 09:22:46 +0100

    vote us

--- vote_us.php
+++ vote_us.php
@@ -17,19 +17,17 @@ $campaign_title = isset($_GET["campaign_title"]) ? $_GET["campaign_title"] : '';
 if ($session -> value('user_id') && $campaign_id) {
 	require_once (dirname(__FILE__) . '/includes/class_project_votes.php');
 	$projectVotes = new projectVotes($session -> value('user_id'), $campaign_id);
-	//$vote_us = $projectVotes -> vote();
-	//if (isset($vote_us["success"]) && $vote_us["success"]) {
-		//$user_voted_email = $projectVotes -> getUserEmail();
-		//$campaign_owner_email = $projectVotes -> getCampaignOwnerEmail();
-		$user_voted_email = "anthony.puggioni2@gmail.com";
-		$campaign_owner_email = "anthony.puggioni2@gmail.com";
-		
+	$vote_us = $projectVotes -> vote();
+	if (isset($vote_us["success"]) && $vote_us["success"]) {
+		$user_voted_email = $projectVotes -> getUserEmail();
+		$campaign_owner_email = $projectVotes -> getCampaignOwnerEmail();
+
 		if (!defined('INCLUDED')) {
 			define('INCLUDED', 1);
 		}
 		include ('language/' . $setts['site_lang'] . '/mails/voting_campaign_owner_notification.php');
 		include ('language/' . $setts['site_lang'] . '/mails/voting_user_notification.php');
-	//}
+	}
 }
 
 echo json_encode($vote_us);
