d14bb544063e6e8d76829f6384a17eefec82aef9
Mark White <mwhite@bringitlocal.com>
2013-11-08 17:46:22 -0800

    edits to the update notification email

--- language/english/mails/campaign_update_user_notification.php
+++ language/english/mails/campaign_update_user_notification.php
@@ -32,19 +32,19 @@ if ($funders_users && count($funders_users) ) {
         $m_subject = "You’ve Received a Campaign Update!";
 
         $m_html_body = 'Hello ' . $fuser['first_name'] ." ". $fuser['last_name'] . ",<br><br>\r\n\r\n";
-        $m_html_body.= "You've received an update from the campaign " . $u_campaign['project_title'] . "<br><br>\r\n\r\n";
+        $m_html_body.= "The Bring It Local community crowdfunding campaign that you support," . $u_campaign['project_title'] . "has posted an update to their campaign page.<br>\r\n\r\n";
         $m_html_body.= $html_msg;
         $m_html_body.= "<br><br>\r\n\r\n";
-        $m_html_body.= "Visit the " . $u_campaign['project_title'] ."<br>\r\n";
-        $m_html_body.= "Respond directly to the campaign owner <a href='".SITE_PATH."about_me.php?user_id=".$u_user['id']."'>".$u_user['first_name'] ." ".$u_user['last_name']."</a>";
+        $m_html_body.= "Check out the update on the campaign page here - " . $u_campaign['project_title'] ."<br>\r\n";
+        $m_html_body.= "See the profile page of the owner of this campaign here - <a href='".SITE_PATH."about_me.php?user_id=".$u_user['id']."'>".$u_user['first_name'] ." ".$u_user['last_name']."</a>";
 
 
         $m_text_body = 'Hello ' . $fuser['first_name'] . " " . $fuser['last_name'] . ",\r\n\r\n";
-        $m_text_body .= "You've received an update from the campaign " . $u_campaign['project_title'] . "\r\n\r\n";
+        $m_text_body .= "The Bring It Local community crowdfunding campaign that you support - " . $u_campaign['project_title'] . "has posted an update to their campaign page. \r\n\r\n";
         $m_text_body .= strip_tags($html_msg);
         $m_text_body .= "\r\n\r\n";
-        $m_text_body .= "Visit the " . $u_campaign['project_title'] . "\r\n";
-        $m_text_body .= "Respond directly to the campaign owner " . SITE_PATH . "about_me.php?user_id=" . $u_user['id'];
+        $m_text_body .= "Check out the update on the campaign page here - " . $u_campaign['project_title'] . "\r\n";
+        $m_text_body .= "See the profile page of the owner of this campaign here - " . SITE_PATH . "about_me.php?user_id=" . $u_user['id'];
 
 
 
