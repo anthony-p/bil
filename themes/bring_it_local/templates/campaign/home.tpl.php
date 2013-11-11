73bb02fadfec0ae6dfd3e99978c5dbadff156ffc
Alexey Dudich <dudich.dev@gmail.com>
2013-11-11 12:59:08 +0200

    create and edit campaign - inappropiate characters in Story Text

--- themes/bring_it_local/templates/campaign/home.tpl.php
+++ themes/bring_it_local/templates/campaign/home.tpl.php
@@ -95,7 +95,7 @@
 
 <aside class="info">
     <div class="inner">
-        <p><?=html_entity_decode($compaignData["campaign_basic"])?></p>
+        <p><?=($compaignData["campaign_basic"])?></p>
     </div>
 </aside>
 
