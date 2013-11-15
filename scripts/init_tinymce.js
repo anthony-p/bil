3164d6b992ff480f567b41d6d33f2b5381e5383d
Anastasia Batieva <batieva.al@gmail.com>
2013-11-15 12:49:44 -0600

    https://oaktree.teamworkpm.net/tasks/2047339
    html is being displayed as code not being parsed on campaign pages

--- scripts/init_tinymce.js
+++ scripts/init_tinymce.js
@@ -21,8 +21,8 @@ function init_tinymce(selector) {
         toolbar_items_size: 'small',
         style_formats: [
             {title: 'Paragraph', block: 'p'},
-            {title: 'Header 1', block: 'span', styles: {color: '#444', font: "1.5em OpenSans, sans-serif", margin: '0 0 1em 0',display:'block'}},
-            {title: 'Header 2', block: 'span', styles: {color: '#444', font: "bold 1.3em OpenSans, sans-serif", margin: '0 0 1em 0',display:'block'}}
+            {title: 'Header 1', block: 'span', styles: {color: '#444', font: "27px OpenSans, sans-serif", margin: '0 0 1em 0',display:'block'}},
+            {title: 'Header 2', block: 'span', styles: {color: '#444', font: "bold 17px OpenSans, sans-serif", margin: '0 0 1em 0',display:'block'}}
 
         ],
         content_css : "/themes/bring_it_local/tinymce.css",
